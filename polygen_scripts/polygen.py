f171910629573b3100dc0946732d8c36c25a3a4e
urquizag <urquizag@hhu.de>
2020-07-17 09:38:40 +0200

    Simplification of polygen architecture

--- polygen_scripts/polygen.py
+++ polygen_scripts/polygen.py
@@ -1,12 +1,23 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template, request
 
-application = Flask(__name__)
+app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return render_template("index.html", content="Testing")
 
-#we need to genearate the correct credentials for logging into synbiohub.
-@application.route('/')
-def hello_world():
-    return 'hello, world I am polygen!!'
+@app.route("/sequence", methods=["POST","GET"])
+def sequence():
+    if request.method == "POST":
+        sequence = request.form["sequence"]
+        return redirect(url_for("success"))
+    else:
+        return render_template("sequence.html")
+
+@app.route("/sucess")
+def success():
+    return "Sucess"
+
 
 if __name__=='__main__':
-    application.run(debug=True, host='0.0.0.0', port=5000) ##host 0.0.0.0 is important for docker container
+    app.run(debug=True, host='0.0.0.0', port=5000) ##host 0.0.0.0 is important for docker container
