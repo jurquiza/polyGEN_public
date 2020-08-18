e85f1bef7a89a442a14be6677638776d9418084d
chwag <chwag108@hhu.de>
2020-08-18 10:58:15 +0200

    Improved overhang selection to prefer overhang set, which features innermost overhangs within each set of overhang sets of equal length

--- polygen_scripts/engine_v2_2.py
+++ polygen_scripts/engine_v2_2.py
@@ -94,7 +94,7 @@ def flattn(list_2d):
 
 
 #Optimizes the overhangs used in Golden Gate assembly
-def golden_gate_optimization(parts_list,gg_overhangs):
+def golden_gate_optimization(parts_list,free_overhangsets):
 
     # Write all variable sequences in same order in a new list
     oh_list = []
@@ -126,53 +126,54 @@ def golden_gate_optimization(parts_list,gg_overhangs):
                     cov_list.append(oh)
                 else:
                     cov_list.append(oh[int(np.ceil(np.true_divide(len(oh),2)))-cov:int(np.ceil(np.true_divide(len(oh),2)))+cov])
-        
+
         # Find possible overhang combinations
-        golden_gate_overhangs=gg_overhangs
-        seq_matches = []
-        for x in range(len(parts_list)-1):
-            seq_matches.append([])
-            for overhang in golden_gate_overhangs:
-                if parts_list[x].type == 'pegRNA':
-                    if overhang in cov_list[x][1]:
-                        seq_matches[x].append(oh_list[x][1][:oh_list[x][1].find(overhang)+4])
-                elif parts_list[x].type == 'gRNA':
-                    if parts_list[x+1].type == 'gRNA':
-                        if overhang in cov_list[x+1]:
-                            seq_matches[x].append(oh_list[x+1][:oh_list[x+1].find(overhang)+4])
-                    elif parts_list[x+1].type == 'pegRNA':
-                        if overhang in cov_list[x+1][0]:
-                            seq_matches[x].append(oh_list[x+1][0][:oh_list[x+1][0].find(overhang)+4])
-                    elif parts_list[x+1].type == 'smRNA':
-                        if overhang in cov_list[x+1]:
-                            seq_matches[x].append(oh_list[x+1][:oh_list[x+1].find(overhang)+4])
-                elif parts_list[x].type == 'smRNA':
-                    if parts_list[x+1].type == 'gRNA':
-                        if overhang in cov_list[x+1]:
-                            seq_matches[x].append(oh_list[x+1][:oh_list[x+1].find(overhang)+4])
-                    elif parts_list[x+1].type == 'pegRNA':
-                        if overhang in cov_list[x+1][0]:
-                            seq_matches[x].append(oh_list[x+1][0][:oh_list[x+1][0].find(overhang)+4])
-                    elif parts_list[x+1].type == 'smRNA':
-                        if overhang in cov_list[x+1]:
-                            seq_matches[x].append(oh_list[x+1][:oh_list[x+1].find(overhang)+4])
-                else: # if part is tRNA
-                    if parts_list[x+1].type == 'gRNA':
-                        if overhang in cov_list[x+1]:
-                            seq_matches[x].append(oh_list[x+1][:oh_list[x+1].find(overhang)+4])
-                    elif parts_list[x+1].type == 'pegRNA':
-                        if overhang in cov_list[x+1][0]:
-                            seq_matches[x].append(oh_list[x+1][0][:oh_list[x+1][0].find(overhang)+4])
-                    elif parts_list[x+1].type == 'smRNA':
-                        if overhang in cov_list[x+1]:
-                            seq_matches[x].append(oh_list[x+1][:oh_list[x+1].find(overhang)+4])
-                  
-        combs = []
-        for x in itertools.product(*seq_matches):
-            combs.append(x)
-        for comb in combs:
-            if len([c[-4:] for c in comb]) == len(set([c[-4:] for c in comb])):
-                return comb
+        for s in free_overhangsets:
+            golden_gate_overhangs=s
+            seq_matches = []
+            for x in range(len(parts_list)-1):
+                seq_matches.append([])
+                for overhang in golden_gate_overhangs:
+                    if parts_list[x].type == 'pegRNA':
+                        if overhang in cov_list[x][1]:
+                            seq_matches[x].append(oh_list[x][1][:oh_list[x][1].find(overhang)+4])
+                    elif parts_list[x].type == 'gRNA':
+                        if parts_list[x+1].type == 'gRNA':
+                            if overhang in cov_list[x+1]:
+                                seq_matches[x].append(oh_list[x+1][:oh_list[x+1].find(overhang)+4])
+                        elif parts_list[x+1].type == 'pegRNA':
+                            if overhang in cov_list[x+1][0]:
+                                seq_matches[x].append(oh_list[x+1][0][:oh_list[x+1][0].find(overhang)+4])
+                        elif parts_list[x+1].type == 'smRNA':
+                            if overhang in cov_list[x+1]:
+                                seq_matches[x].append(oh_list[x+1][:oh_list[x+1].find(overhang)+4])
+                    elif parts_list[x].type == 'smRNA':
+                        if parts_list[x+1].type == 'gRNA':
+                            if overhang in cov_list[x+1]:
+                                seq_matches[x].append(oh_list[x+1][:oh_list[x+1].find(overhang)+4])
+                        elif parts_list[x+1].type == 'pegRNA':
+                            if overhang in cov_list[x+1][0]:
+                                seq_matches[x].append(oh_list[x+1][0][:oh_list[x+1][0].find(overhang)+4])
+                        elif parts_list[x+1].type == 'smRNA':
+                            if overhang in cov_list[x+1]:
+                                seq_matches[x].append(oh_list[x+1][:oh_list[x+1].find(overhang)+4])
+                    else: # if part is tRNA
+                        if parts_list[x+1].type == 'gRNA':
+                            if overhang in cov_list[x+1]:
+                                seq_matches[x].append(oh_list[x+1][:oh_list[x+1].find(overhang)+4])
+                        elif parts_list[x+1].type == 'pegRNA':
+                            if overhang in cov_list[x+1][0]:
+                                seq_matches[x].append(oh_list[x+1][0][:oh_list[x+1][0].find(overhang)+4])
+                        elif parts_list[x+1].type == 'smRNA':
+                            if overhang in cov_list[x+1]:
+                                seq_matches[x].append(oh_list[x+1][:oh_list[x+1].find(overhang)+4])
+
+            combs = []
+            for x in itertools.product(*seq_matches):
+                combs.append(x)
+            for comb in combs:
+                if len([c[-4:] for c in comb]) == len(set([c[-4:] for c in comb])):
+                    return comb
     #if there are no possible combinations
     return None
 
@@ -183,7 +184,7 @@ scaffld = 'GTTTTAGAGCTAGAAATAGCAAGTTAAAATAAGGCTAGTCCGTTATCAACTTGAAAAAGTGGCACCGAG
 
 
 # Perform scarless Golden Gate assembly computation
-def scarless_gg(parts_list, primer_tm_range=[52,60], max_annealing_len=30, bb_overlaps=['ggca','gttt'], additional_overhangs=[]):
+def scarless_gg(parts_list, primer_tm_range=[52,60], max_annealing_len=30, bb_overlaps=['tgcc','gttt'], additional_overhangs=[]):
     bb_overlaps = [i.lower() for i in bb_overlaps]
     additional_overhangs = [i.lower() for i in additional_overhangs]
     
@@ -214,11 +215,12 @@ def scarless_gg(parts_list, primer_tm_range=[52,60], max_annealing_len=30, bb_ov
         gg_opt = None
         breakit = False
         for p in range(10,51):
+            free_overhangsets = []
             for q in range(5):
                 with open('overhangsets/setsof%s.csv'%p,'r') as f:
                     reader = csv.reader(f, delimiter=",")
                     sets = list(reader)[1:]
-
+                
                 temp = []
                 for s in sets:
                     if len(s) != 0:
@@ -226,52 +228,51 @@ def scarless_gg(parts_list, primer_tm_range=[52,60], max_annealing_len=30, bb_ov
 
                 # Only grab sets that include all existing overhangs and delete the existing from the set
                 if all(i in [x.lower() for x in temp[q]] for i in additional_overhangs+bb_overlaps):
-                    free_overhangs = [i for i in [x.lower() for x in temp[q]] if i not in additional_overhangs+bb_overlaps]
+                    free_overhangsets.append([i for i in [x.lower() for x in temp[q]] if i not in additional_overhangs+bb_overlaps])
                 else:
                     continue
-                
-                gg_overhangs = []
-                for x in free_overhangs:
-                    gg_overhangs.append(x)
-                gg_opt = golden_gate_optimization(unpacked_list,gg_overhangs)
-                if gg_opt is not None:
-                    breakit = True
-                if breakit:
-                    break
+            
+            if free_overhangsets:
+                gg_opt = golden_gate_optimization(unpacked_list,free_overhangsets)
+            if gg_opt is not None:
+                breakit = True
             if breakit:
                 break
         
         # No sets include all existing overhangs
         if gg_opt is None:
+            
             warnings.warn('The given combination of existing overhangs is not compatible with an optimal overhang set. '
                           'Computing the best overhang set not including the given existing overhangs. There might be '
                           'interference between overhangs.')
+            
             for p in range(10,51):
+                free_overhangsets = []
                 for q in range(5):
                     with open('overhangsets/setsof%s.csv'%p,'r') as f:
                         reader = csv.reader(f, delimiter=",")
-                        temp = list(reader)[1:]
+                        sets = list(reader)[1:]
 
-                    # Only grab sets that include no existing overhangs
-                    if any(i in [x.lower() for x in temp[q]] for i in additional_overhangs+bb_overlaps):
-                        continue
+                    temp = []
+                    for s in sets:
+                        if len(s) != 0:
+                            temp.append(s)
+
+                    # Only grab sets that include all existing overhangs and delete the existing from the set
+                    if all(i in [x.lower() for x in temp[q]] for i in additional_overhangs+bb_overlaps):
+                        free_overhangsets.append([i for i in [x.lower() for x in temp[q]] if i not in additional_overhangs+bb_overlaps])
                     else:
-                        free_overhangs = [x.lower() for x in temp[q]]
+                        continue
 
-                    gg_overhangs = []
-                    for x in free_overhangs:
-                        gg_overhangs.append(x)
-                    gg_opt = golden_gate_optimization(unpacked_list,gg_overhangs)
-                    if gg_opt is not None:
-                        breakit = True
-                    if breakit:
-                        break
+                gg_opt = golden_gate_optimization(unpacked_list,free_overhangsets)
+                if gg_opt is not None:
+                    breakit = True
                 if breakit:
                     break
         
         if gg_opt is None:
             raise ValueError('The given combination of existing overhangs does not allow for an optimal overhang set.')
-                
+
         #Modify sequences and design primers
         for i in range(len(unpacked_list)):
             
@@ -352,7 +353,7 @@ def scarless_gg(parts_list, primer_tm_range=[52,60], max_annealing_len=30, bb_ov
                         unpacked_list[i+1].primer_forward = "taggtctcc" + unpacked_list[i+1].sequence[unpacked_list[i+1].sequence.find(gg_opt[i])+len(gg_opt[i])-4:unpacked_list[i+1].sequence.find(scaffld.lower())+max_annealing_len]
                         unpacked_list[i+1].sequence = "taggtctcc" + unpacked_list[i+1].sequence[unpacked_list[i+1].sequence.find(gg_opt[i])+len(gg_opt[i])-4:]
         new_builds_list.append(unpacked_list)
-
+    
     #Optimize primer Tm
     for unpacked_list in new_builds_list:
         for part in unpacked_list:
