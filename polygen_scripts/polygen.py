ca184705fd15cd22f8d381da168e31d6324292dc
chwag <chwag108@hhu.de>
2021-01-06 12:01:39 +0100

    The pages now save input information when the user causes errors or switches between pages. Added a reset button for the PTG page.

--- polygen_scripts/polygen.py
+++ polygen_scripts/polygen.py
@@ -1,9 +1,9 @@
-from flask import Flask, redirect, url_for, render_template, request, Response#, session
+from flask import Flask, redirect, url_for, render_template, request, Response
 import os
 
 from engine_v2_2 import *
     
-session = {'clr': {'link': '#FF0000'}}		# dictionary to pass variables around routes
+session = {}		# dictionary to pass variables around routes
 
 
 app = Flask(__name__)
@@ -24,45 +24,50 @@ def sequence():
     session['clr'] = {'sequence_spacers': '#FFFFFF', 'link': '#FFFFFF'}
     
     if request.method == "POST":
-        runall_args = {}
-        runall_args['poltype_run'] = request.form["poltype_input"]
-        PTG_input = request.form["sequence_spacers"]
-        session['PTG_transfer'] = PTG_input
-        session['PTG_oligo'] = request.form["oligo_prefix"]
-        if request.form["oligo_prefix"]:
-            session['PTG_oligo'] = request.form["oligo_prefix"]
-        else:
-            session['PTG_oligo'] = 'o'
-        if request.form["oligo_index"]:
-            session['oligo_index'] = request.form["oligo_index"]
-        else:
-            session['oligo_index'] = '0'
-        if request.form["PTG_name"]:
+        if request.form['submit_button'] == 'submit':
+            runall_args = {}
+            runall_args['poltype_run'] = request.form["poltype_input"]
             session['PTG_name'] = request.form["PTG_name"]
-        else:
-            session['PTG_name'] = 'PTG'
-        runall_args['tm_range'] = [int(request.form['min_temp'][:2]), int(request.form['max_temp'][:2])]
-        if request.form['bb_ovrhng']:
-            runall_args['bb_overlaps'] = request.form['bb_ovrhng'].split(';')
-        if request.form['add_ovrhng']:
-            runall_args['additional_overhangs'] = request.form['add_ovrhng'].split(';')
-        PTG_input = PTG_input.split('|')
-        PTG_structure = []
-        PTG_index=1
-        for element in PTG_input:
-            element_list=[]
-            element_list.append(session['PTG_oligo']+'_'+str(PTG_index))
-            PTG_index+=1
-            for e in element.split(';'):
-                element_list.append(e)
-            PTG_structure.append(element_list)  
-
-        session['out'],ftrs,session['msg'],session['primers'] = runall(PTG_structure, **runall_args)
-        if session['msg'] == 'comb_error':
-            return render_template("sequence.html", PTG_transfer=session.get('PTG_transfer', None), session=session)
+            session['oligo_prefix'] = request.form["oligo_prefix"]
+            session['oligo_index'] = request.form["oligo_index"]
+            session['PTG_transfer'] = request.form["sequence_spacers"]
+            session['bb_ovrhng'] = request.form["bb_ovrhng"]
+            session['add_ovrhng'] = request.form["add_ovrhng"]
         
-        return render_template("primer_list.html", session=session)
+            runall_args['tm_range'] = [int(request.form["min_temp"][:2]), int(request.form["max_temp"][:2])]
+            if session['bb_ovrhng']:
+                runall_args['bb_overlaps'] = session["bb_ovrhng"].split(';')
+            if session['add_ovrhng']:
+                runall_args['additional_overhangs'] = session["add_ovrhng"].split(';')
+            
+            PTG_input = session['PTG_transfer'].split('|')
+            PTG_structure = []
+            PTG_index=1
+            for element in PTG_input:
+                element_list=[]
+                element_list.append([session['oligo_prefix'] if session['oligo_prefix'] else 'o'][0]+'_'+str(PTG_index))
+                PTG_index+=1
+                for e in element.split(';'):
+                    element_list.append(e)
+                PTG_structure.append(element_list)  
 
+            session['out'],ftrs,session['msg'],session['primers'] = runall(PTG_structure, **runall_args)
+            if session['msg'] == 'comb_error':
+                return render_template("sequence.html", PTG_transfer=session.get('PTG_transfer', None), session=session)
+        
+            if not session['PTG_name']:
+                session['PTG_name'] = 'PTG'
+            if not session['oligo_prefix']:
+                session['oligo_prefix'] = 'o'
+            if not session['oligo_index']:
+                session['oligo_index'] = '0'
+        
+            return render_template("primer_list.html", session=session)
+            
+        elif request.form['submit_button'] == 'reset':
+            session['PTG_name'] = session['oligo_prefix'] = session['oligo_index'] = session['bb_ovrhng'] = session['add_ovrhng'] = session['PTG_transfer'] = ''
+            return render_template("sequence.html",PTG_transfer=session.get('PTG_transfer', None), session=session)
+            
     else:
         
         return render_template("sequence.html",PTG_transfer=session.get('PTG_transfer', None), session=session)
@@ -73,15 +78,16 @@ def peg_generation():
     session['clr'] = {'sequence': '#FFFFFF', 'edits': '#FFFFFF'}
 
     if request.method == "POST":
-        PEG_sequence = request.form["sequence"]
-        PEG_edits = request.form["edits"].split('|')
+        session['PEG_sequence'] = request.form["sequence"]
+        session['PEG_edits'] = request.form["edits"]
+        PEG_edits = session['PEG_edits'].split('|')
         PEG_mode = request.form["mode"]
 
         pegs_list = []
         for edt in PEG_edits:
             pegs_list.append(edt.split(';'))
 
-        edits_list = pegbldr(PEG_sequence, pegs_list, PEG_mode)
+        edits_list = pegbldr(session['PEG_sequence'], pegs_list, PEG_mode)
 
         session['PTG_transfer'] = ''
         for c,peg in enumerate(edits_list):
@@ -91,7 +97,7 @@ def peg_generation():
 
         return redirect(url_for('sequence'))
     else:
-        return render_template("peg_generation.html", session=session)
+        return render_template("peg_generation.html", PEG_transfer=session.get('PEG_sequence', None), session=session)
         
 @app.route("/primer_list", methods=["POST","GET"])
 def serve_primers():
@@ -101,8 +107,8 @@ def serve_primers():
     collapsed_index = int(session['oligo_index'])
     oligo_ids = []
     for primer in session['primers']:
-        oligo_ids.append(session['PTG_oligo']+format(collapsed_index,'0'+str(positions)))
-        csv += session['PTG_oligo']+format(collapsed_index,'0'+str(positions))+','+primer+'\n'
+        oligo_ids.append(session['oligo_prefix']+format(collapsed_index,'0'+str(positions)))
+        csv += session['oligo_prefix']+format(collapsed_index,'0'+str(positions))+','+primer+'\n'
         collapsed_index += 1
     
     csv += '\nTable of fragments:\n'
@@ -125,7 +131,7 @@ def success():
 def handle_invalid_usage(error):
     session['msg'] = error.to_dict()['message']
     session['clr'][error.to_dict()['box']] = '#FF0000'
-    return render_template(error.to_dict()['pge'], PTG_transfer=session.get('PTG_transfer', None), session=session)
+    return render_template(error.to_dict()['pge'], PEG_transfer=session.get('PEG_sequence'), PTG_transfer=session.get('PTG_transfer', None), session=session)
 
 
 if __name__=='__main__':
